fe416dd765e353399917a7be8c0d2ba00e7ae053
anclaev <iahugo@yandex.ru>
2021-02-01 00:37:28 +0400

    add: images & pre-header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <header className="header">
+      <img src="img/logo.svg" alt="Logo Space X" className="logo" />
+      <nav className="main-nav nav">
+        <ul className="list">
+          <li className="item">
+            <a href="#" className="item-link">
+              Falcon 1
+            </a>
+          </li>
+          <li className="item">
+            <a href="#" className="item-link">
+              Falcon 9
+            </a>
+          </li>
+          <li className="item">
+            <a href="#" className="item-link">
+              Falcon Heavy
+            </a>
+          </li>
+          <li className="item">
+            <a href="#" className="item-link">
+              Updates
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <nav className="secondary-nav">
+        <ul className="list">
+          <li className="item">
+            <a href="#" className="item-link">
+              Home
+            </a>
+          </li>
+          <li className="item">
+            <a href="calendar.html" className="item-link">
+              Calendar
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default App;
