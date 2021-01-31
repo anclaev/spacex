2232e1c9ac35d2c494d9b2e1fd1abf0ac5311788
anclaev <iahugo@yandex.ru>
2021-02-01 01:10:54 +0400

    add: section "main" as component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import "./css/style.css";
 
 function App() {
   return (
-    <React.Fragment>
+    <>
       <Header />
-      <section className="main">
-        <h1 className="title">Falcon 1</h1>
-
-        <div className="video-container">
-          <video
-            className="video"
-            autoPlay
-            loop
-            muted
-            src="./video/moon.mp4"
-          ></video>
-        </div>
-      </section>
-    </React.Fragment>
+      <Main />
+    </>
   );
 }
 
