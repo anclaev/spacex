90fdb23415559e669db866e00e32a9240a05171f
anclaev <iahugo@yandex.ru>
2021-02-01 01:42:40 +0400

    add: section "calendar" as component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
+import Calendar from "./components/Calendar";
 
 import "./css/style.css";
 
