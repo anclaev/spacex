a54e3b81f9156c0e8505773806a475635fe29644
anclaev <iahugo@yandex.ru>
2021-02-01 01:33:29 +0400

    add: footer as component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Features from "./components/Features";
+import Footer from "./components/Footer";
+
 import "./css/style.css";
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Header />
       <Main />
       <Features />
+      <Footer />
     </>
   );
 }
