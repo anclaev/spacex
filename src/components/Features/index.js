e3ad6c0b8b144f982c2568aa9ff923882fec08cf
anclaev <iahugo@yandex.ru>
2021-02-01 19:56:14 +0400

    fix: rounting

--- src/components/Features/index.js
+++ src/components/Features/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import RellaxWrapper from "react-rellax-wrapper";
 import "./features.css";
 
+import Main from "../Main";
+
 const rocketImages = {
   "Falcon 1": "falcon-1",
   "Falcon 9": "falcon-9",
@@ -18,61 +20,64 @@ const Features = ({
   description,
 }) => {
   return (
-    <section className="features">
-      <h2 className="features-title">
-        {name}
-        <br />
-        Overview
-      </h2>
-      <div className="overview">
-        <table className="table">
-          <caption className="table-title">Size</caption>
-          <thead>
-            <tr>
-              <td className="table-column">HEIGHT</td>
-              <td className="table-column">
-                {height.meters} m / {height.feet} ft
-              </td>
-            </tr>
-            <tr>
-              <td className="table-column">DIAMETER</td>
-              <td className="table-column">
-                {diameter.meters} m / {diameter.feet} ft
-              </td>
-            </tr>
-            <tr>
-              <td className="table-column">MASS</td>
-              <td className="table-column">
-                {mass.kg} kg / {mass.lb} lb
-              </td>
-            </tr>
-
-            {payloadWeights.map((item) => (
-              <tr key={item.id}>
+    <>
+      <Main rocket={name} />
+      <section className="features">
+        <h2 className="features-title">
+          {name}
+          <br />
+          Overview
+        </h2>
+        <div className="overview">
+          <table className="table">
+            <caption className="table-title">Size</caption>
+            <thead>
+              <tr>
+                <td className="table-column">HEIGHT</td>
                 <td className="table-column">
-                  PAYLOAD TO {item.id.toUpperCase()}
-                </td>
-                <td className="table-column">
-                  {item.kg} kg / {item.lb} lb
+                  {height.meters} m / {height.feet} ft
                 </td>
               </tr>
-            ))}
-          </thead>
-        </table>
-        <RellaxWrapper speed="10">
-          <img
-            src={`img/${rocketImages[name]}.png`}
-            alt="rocket"
-            className="rocket"
-          />
-        </RellaxWrapper>
+              <tr>
+                <td className="table-column">DIAMETER</td>
+                <td className="table-column">
+                  {diameter.meters} m / {diameter.feet} ft
+                </td>
+              </tr>
+              <tr>
+                <td className="table-column">MASS</td>
+                <td className="table-column">
+                  {mass.kg} kg / {mass.lb} lb
+                </td>
+              </tr>
 
-        <article>
-          <h3 className="features-subtitle">DESCRIPTION</h3>
-          <p className="features-text">{description}</p>
-        </article>
-      </div>
-    </section>
+              {payloadWeights.map((item) => (
+                <tr key={item.id}>
+                  <td className="table-column">
+                    PAYLOAD TO {item.id.toUpperCase()}
+                  </td>
+                  <td className="table-column">
+                    {item.kg} kg / {item.lb} lb
+                  </td>
+                </tr>
+              ))}
+            </thead>
+          </table>
+          <RellaxWrapper speed="10">
+            <img
+              src={`img/${rocketImages[name]}.png`}
+              alt="rocket"
+              className="rocket"
+            />
+          </RellaxWrapper>
+
+          <article>
+            <h3 className="features-subtitle">DESCRIPTION</h3>
+            <p className="features-text">{description}</p>
+          </article>
+        </div>
+      </section>
+    </>
   );
 };
 
